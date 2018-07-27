b1ea1b789801ca9a51906006f717b58249506aed
JanedoeGVA <xavier.costa@unige.ch>
2018-07-27 15:06:47 +0200

    modifiy dockerfile script path

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM alpine
 
 RUN apk add --update openssl && \
+apk add --no-cache bash && \
 rm -rf /var/cache/apk*
 
 COPY docker-entrypoint.sh /run/script/docker-entrypoint.sh
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,7 @@
-#!/bin/sh
-chmod a+w $DHPARAM
+#!/bin/bash
+chmod a+w /etc/ssl/dh-param
 
 if [ ! -f $DHPARAM]; then
     echo -e "not present, creating dh-param im secret volume"
     openssl dhparam -out $DHPARAM 2048
 fi
-
-exec "$@"
