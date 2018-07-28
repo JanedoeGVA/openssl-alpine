b3edfa4422e18abd0f11c13321d18e52d56e934b
JanedoeGVA <xavier.costa@unige.ch>
2018-07-28 19:06:31 +0200

    test without openssl

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM alpine
 
-RUN apk add --update openssl && \
-apk add --no-cache bash && \
-rm -rf /var/cache/apk*
-
 COPY docker-entrypoint.sh /run/script/docker-entrypoint.sh
 
 RUN ["chmod", "+x", "/run/script/docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 chmod a+w /etc/ssl/dh-param
 
-if [ ! -f $DHPARAM]; then
+#if [ ! -f $DHPARAM]; then
     echo -e "not present, creating dh-param im secret volume"
-    openssl dhparam -out $DHPARAM 2048
-fi
+    #openssl dhparam -out $DHPARAM 2048
+#fi
