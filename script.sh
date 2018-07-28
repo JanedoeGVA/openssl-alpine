d58830f5977e8b75e8e4cdf5fd99de1f7dad0993
JanedoeGVA <xavier.costa@unige.ch>
2018-07-28 21:06:16 +0200

    testing script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 chmod a+w /etc/ssl/dh-param
 
-if [ ! -f /etc/ssl/dh-param/dhparam-2048.pem]; then
+#if [ ! -f /etc/ssl/dh-param/dhparam-2048.pem]; then
     echo -e "not present, creating dh-param im secret volume"
     #openssl dhparam -out $DHPARAM 2048
-fi
+#fi
