3b6246f859e20236802fa1803a3a70087cb619fc
JanedoeGVA <xavier.costa@unige.ch>
2018-07-30 10:08:14 +0200

    testing script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 chmod a+w $DH_PARAM_PATH
 
-if [ ! -f $DH_PARAM_PATH/test.txt]; then
+if [ ! -f $DH_PARAM_PATH/test2.txt]; then
     echo -e "not present, creating dh-param im secret volume"
+    touch $DH_PARAM_PATH/test2.txt
     #openssl dhparam -out $DH_PARAM_PATH/dhparam-2048.pem 2048
 else 
-    echo -2 "present !!!"
+    echo "present !!!"
 fi
