6040167d91c6eb5778068a1d7ec60207c1088b5d
JanedoeGVA <xavier.costa@unige.ch>
2018-07-31 11:35:28 +0200

    modify script

--- script.sh
+++ script.sh
@@ -1,21 +1,8 @@
 #!/bin/sh
 
-[ -f $DH_PARAM_PATH/test2.txt ] || echo "File test2.txt not found!"
-[ -f $DH_PARAM_PATH/test3.txt ] || echo "File test3.txt not found!"
-[ ! -f $DH_PARAM_PATH/test2.txt ] || echo "File test2.txt found!"
-[ ! -f $DH_PARAM_PATH/test3.txt ] || echo "File test3.txt found!"
-echo $DH_PARAM_PATH/test2.txt
-
-bool=true
-
-if [ "$bool" = true ]; then
- echo -e "true"
-fi
-
-if [ ! -f $DH_PARAM_PATH/test3.txt ]; then
- echo -e "IS NOT !!!"
- echo -e "creating"
- touch $DH_PARAM_PATH/test3.txt
+if [ ! -f $DH_PARAM_PATH/dhpram-2048.pem ]; then
+ echo -e " Creating DH-PARAM..."
+ openssl dhparam -out $DH_PARAM_PATH/dhparam-2048.pem 2048
 fi
 
 sleep 50m
