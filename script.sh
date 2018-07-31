d337bf2e700c2da1cf174c4717524d7d9de347eb
JanedoeGVA <xavier.costa@unige.ch>
2018-07-31 09:40:14 +0200

    modify script

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@
 if [ ! -f /etc/ssl/dh-param/test2.txt]; then
     echo -e "not present, creating dh-param im secret volume"
     touch /etc/ssl/dh-param/test2.txt
-    #openssl dhparam -out $DH_PARAM_PATH/dhparam-2048.pem 2048
 fi
 
 sleep 50m
