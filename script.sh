a8087da512c67d960b4e7e8a29e45808c67ec184
JanedoeGVA <xavier.costa@unige.ch>
2018-07-31 10:05:14 +0200

    modify script

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-[ -f $DH_PARAM_PATH/test2.txt ] || echo "File not found!"
-[ -f $DH_PARAM_PATH/test3.txt ] || echo "File not found!"
+[ -f $DH_PARAM_PATH/test2.txt ] || echo "File test2.txt not found!"
+[ -f $DH_PARAM_PATH/test3.txt ] || echo "File test3.txt not found!"
+[ ! -f $DH_PARAM_PATH/test2.txt ] || echo "File test2.txt found!"
+[ ! -f $DH_PARAM_PATH/test3.txt ] || echo "File test3.txt found!"
 echo $DH_PARAM_PATH/test2.txt
 
 if [ ! -f /etc/ssl/dh-param/test2.txt]; then
