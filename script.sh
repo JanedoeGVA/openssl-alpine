e9461aae4cf76403a6317d23e24e7ff0256aa6a7
JanedoeGVA <xavier.costa@unige.ch>
2018-07-31 09:45:48 +0200

    modify script

--- script.sh
+++ script.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+[ -f $DH_PARAM_PATH/text2.txt ] || echo "File not found!"
+[ -f $DH_PARAM_PATH/text3.txt ] || echo "File not found!"
+echo $DH_PARAM_PATH/text2.txt
+
 if [ ! -f /etc/ssl/dh-param/test2.txt]; then
-    echo -e "not present, creating dh-param im secret volume"
+    echo -e "text 2 not present, creating it"
     touch /etc/ssl/dh-param/test2.txt
+fi
+
+if [ ! -f /etc/ssl/dh-param/test3.txt]; then
+    echo -e "text 3 not present, creating it"
+    touch /etc/ssl/dh-param/test3.txt
 fi
 
 sleep 50m
